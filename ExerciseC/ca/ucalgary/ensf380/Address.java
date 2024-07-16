ecf79b8ecf1bd2cc7fca7b3422ad2a9bfe4286cc
Aroush Qureshi <aroushq1@gmail.com>
2024-07-16 15:35:13 -0600

    File changes made to address and professor classes

--- ExerciseC/ca/ucalgary/ensf380/Address.java
+++ ExerciseC/ca/ucalgary/ensf380/Address.java
@@ -1,4 +1,74 @@
+package ca.ucalgary.ensf380;
 
 public class Address {
-
+	private String street;
+	private String city;
+	private String state;
+	private String postalCode;
+	private String country;
+	
+	//class constructor 
+	public Address(String street, String city, String state, String postalCode, String country) {
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.postalCode = postalCode;
+		this.country = country;
+	}
+	
+	//setters
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	//getters
+	public String getStreet() {
+		return street;
+	}
+	
+	public String getCity() {
+		this.city = city;
+	}
+	
+	public String getState() {
+		this.state = state;
+	}
+	
+	public String getPostalCode() {
+		this.postalCode = postalCode;
+	}
+	
+	public String getCountry() {
+		this.country = country;
+	}
+	
+	public boolean validate() {   // the body of this method was generated using AI but was modified for accurate results.
+		return street != null && !street.isEmpty()
+                && city != null && !city.isEmpty()
+                && state != null && !state.isEmpty()
+                && postalCode != null && !postalCode.isEmpty()
+                && country != null && !country.isEmpty();
+	}
+	
+	public String outputAsLabel() {  //this method returns the full address 
+		return street + ", " + city + ", " + state + ", " + country + ", " +  postalCode;
+	}
+	
+	
 }
--- ExerciseC/ca/ucalgary/ensf380/Professor.java
+++ ExerciseC/ca/ucalgary/ensf380/Professor.java
@@ -1,4 +1,39 @@
+package ca.ucalgary.ensf380;
 
-public class Professor {
+class Professor extends Person {
+	private String teacherNumber;
+	private double salary;
+	
+
+	//constructors
+	public Professor(String name, String phoneNumber, String emailAddress, Address address) {
+		super(name, phoneNumber, emailAddress, address);
+	}
+
+    public Professor(String name, String phoneNumber, String emailAddress, Address address, String teacherNumber, double salary) {
+        super(name, phoneNumber, emailAddress, address);
+        setTeacherNumber(teacherNumber);
+        setSalary(salary);
+    }
+	
+	//setters
+	public void setTeacherNumber(String teacherNumber) {
+		this.teacherNumber = teacherNumber;
+	}
+	
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	//getters
+	public String getTeacherNumber() {
+		return teacherNumber;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
+	
 
 }
